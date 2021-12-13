4e701e8ade02064834e6ba37942b2951f68773ec
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2021-12-13 12:12:56 +0530

    Initial commit

---
+++ src/test/java/com/example/springbootlogging/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
