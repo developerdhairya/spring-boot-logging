10d5acce7c2f8761b7a66bb20763450d67d76f7e
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2021-12-13 12:51:23 +0530

    add parameterized logs

--- src/main/java/com/example/springbootlogging/controller/LogController.java
+++ src/main/java/com/example/springbootlogging/controller/LogController.java
@@ -6,15 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class LogController {
 
     Logger logger= LoggerFactory.getLogger(LogController.class);
 
     @RequestMapping
-    public String makeLogs(){
+    public String makeLogs() throws NoSuchFieldException {
         logger.trace("A trace message");
         logger.debug("A debug message");
-        logger.info("An info message");
+        logger.info("An {}info message {}", LogController.class.getAnnotations());
         logger.warn("A warn message");
         logger.error("An error message");
 
